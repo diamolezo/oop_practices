2e832d77538b5e8c1a2f93e3de4baf744755de84
diamolezo <diamolezo@gmail.com>
2024-05-06 19:52:46 +0300

    create example #2

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -11,3 +11,26 @@ class Animal:
 
 example = Animal(name="Bobik", type="dog", age="5")
 example.make_sounds("gav-gav")
+
+#2
+
+class Book:
+    def __init__(self, name: str, author: str, qty: int):
+        self.name = name
+        self.author = author
+        self.qty = qty
+    def open_book(self, page: int):
+
+        if self.qty >= 1 and self.qty <= page:
+            print(f"Page {page} open in book '{self.name}', {self.author}")
+        else:
+            print(f"Page {page} not open in book '{self.name}', {self.author}")
+
+
+class Program:
+    @staticmethod
+    def main():
+        example = Book(name="1984", author="Джордж Оруэлл", qty=288)
+        example.open_book(int(input("Page >> ")))
+
+Program.main()
