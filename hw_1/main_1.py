277f69c14eacb60208fe564b7016348c2693de01
diamolezo <diamolezo@gmail.com>
2024-05-06 15:09:18 +0300

    check errors

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -1,14 +1,13 @@
 #1
 class Animal:
-    def __init__(self, name, type, age):
-        self.name: str
-        self.type: str
-        self.age: int
+    def __init__(self, name: str, type: str, age: int):
+        self.name = name
+        self.type = type
+        self.age = age
 
     def make_sounds(self, sound: str):
         print(f"Animal {self.type} {self.name} make sounds '{sound}'")
 
 
 example = Animal(name="Bobik", type="dog", age="5")
-print(example)
 example.make_sounds("gav-gav")
