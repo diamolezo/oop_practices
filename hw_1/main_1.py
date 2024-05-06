03efb488c6427f520428e7cbbfb8f803f82ec3eb
diamolezo <diamolezo@gmail.com>
2024-05-06 14:48:01 +0300

    add file main_1

---
+++ hw_1/main_1.py
@@ -0,0 +1,14 @@
+#1
+class Animal:
+    def __init__(self, name, type, age):
+        self.name: str
+        self.type: str
+        self.age: int
+
+    def make_sounds(self, sound: str):
+        print(f"Animal {self.type} {self.name} make sounds '{sound}'")
+
+
+example = Animal(name="Bobik", type="dog", age="5")
+print(example)
+example.make_sounds("gav-gav")
