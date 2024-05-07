c7419114a2b407ae8fc4bc83781a6e003a4af7a1
diamolezo <diamolezo@gmail.com>
2024-05-07 09:51:49 +0300

    fix bags example #2

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -16,21 +16,66 @@ example.make_sounds("gav-gav")
 
 class Book:
     def __init__(self, name: str, author: str, qty: int):
+        '''
+        Конструктор
+
+        :param name:
+        :param author:
+        :param qty:
+        '''
         self.name = name
         self.author = author
         self.qty = qty
-    def open_book(self, page: int):
 
-        if self.qty >= 1 and self.qty <= page:
-            print(f"Page {page} open in book '{self.name}', {self.author}")
+
+    def output_book(self):
+        '''
+          Метод вывода информации о книге
+        :return:
+        '''
+
+        print(f"Книга '{self.name}', автор {self.author}. В книге "
+              f"{self.qty} страниц.\n")
+    def open_book(self, page: int):
+        '''
+        Метод открытия определенной страницы книги
+
+        :param page:
+        :return:
+        '''
+
+        if page >= 1 and page <= self.qty:
+            print(f"Страница {page} успешно открыта в книге '{self.name}', {self.author}\n")
         else:
-            print(f"Page {page} not open in book '{self.name}', {self.author}")
+            print(f"Страницы {page} нет в книге '{self.name}', {self.author}\n")
+
+    def load_menu(self):
+        print(f"Меню действий: 1 - Инфо о книге | 2 - Открыть страницу | 3 - Выход")
 
 
 class Program:
     @staticmethod
     def main():
         example = Book(name="1984", author="Джордж Оруэлл", qty=288)
-        example.open_book(int(input("Page >> ")))
+        while True:
+            Book.load_menu(self=None)
+            try:
+                user_select = int(input("Укажите пункт меню: "))
+            except ValueError:
+                print("Введите число")
+            if 3 <= user_select <= 1:
+                print("Введите число от 1 до 3")
+            match user_select:
+                case 1:
+                    example.output_book()
+                case 2:
+                    example.open_book(int(input("Страница >> ")))
+                case 3:
+                    break
+
+
+
+
+
 
 Program.main()
